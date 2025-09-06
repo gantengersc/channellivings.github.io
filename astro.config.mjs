d98c10915a59a5ec76687732be1462b50df9c4b8
rxhmadi <evelinehurtz@gmail.com>
2025-09-06 14:42:02 +0700

    Setup custom domain: Update Astro config for channelliving.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://gantengersc.github.io/channellivings.github.io/",
-	base: "/channellivings.github.io/",
+	site: "https://channelliving.com/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
